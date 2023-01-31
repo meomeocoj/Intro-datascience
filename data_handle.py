97dd5c468dbe96123c98a6a037635eda9aa98e99
meomeocoj <tminh1103@gmail.com>
2023-01-31 23:43:19 +0700

    add: split data

--- data_handle.py
+++ data_handle.py
@@ -13,15 +13,12 @@ def split_data(df):
     df['Date'] = df['Date'].str.split(' ').str[0]
     df['Date'] = pd.to_datetime(df['Date']).dt.normalize()
     df.sort_values('Date')
-    high_prices = df.loc[:,"High"]
-    low_prices = df.loc[:,"Low"]
-    print(high_prices)
-    print(low_prices)
-    
+
+    high_prices = df.loc[:,"High"].values
+    low_prices = df.loc[:,"Low"].values
     mid_prices = (high_prices + low_prices) / 2.0
-    
-    train_data = mid_prices[:int(df.shape[0]*0.8),:]
-    test_data = mid_prices[int(df.shape[0]*0.8):,:]
+    train_data = mid_prices[:int(df.shape[0]*0.8)].reshape(-1,1)
+    test_data = mid_prices[int(df.shape[0]*0.8):].reshape(-1,1)
 
     return (train_data, test_data)
 
