b4b4d8fd697a4de59a7096e64cf865af2bf26568
meomeocoj <tminh1103@gmail.com>
2023-02-01 06:48:50 +0700

    modified: lstm

--- lstm.py
+++ lstm.py
@@ -1,10 +1,31 @@
 import numpy as np
 import pandas as pd
 import os
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 from data_handle import data_fetching, split_data
+from sklearn.metrics import mean_squared_error
+from utils import plot_predictions_vs_true
+
+def preprocessing_data():
+# Fetch data
+    if not os.path.exists("data/stock_data.csv"):
+        data_fetching('AAPL',start='2010-01-01', end='2022-12-31')
+# read csv
+    df = pd.read_csv('data/stock_data.csv')
+# Split data into train and test
+    train_data, test_data = split_data(df)
+# Normalization
+    scale = MinMaxScaler(feature_range=(0,1))
+    scale_train_data = scale.fit_transform(train_data)
+    scale_test_data = scale.fit_transform(test_data)
+# Incorporating timesteps into data
+    window_size = int(scale_train_data.size*0.3)
+    X_train, y_train = incorporate_timesteps(scale_train_data, window_size = window_size)
+    X_test, y_test = incorporate_timesteps(scale_test_data, window_size = window_size)
+    return X_train, y_train, X_test, y_test
 
 
 def incorporate_timesteps(data,window_size):
@@ -17,39 +38,32 @@ def incorporate_timesteps(data,window_size):
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     return X_train, y_train
 
-# Fetch data
-if not os.path.exists("data/stock_data.csv"):
-    data_fetching('AAPL',start='2010-01-01', end='2022-12-31')
-# read csv
-df = pd.read_csv('data/stock_data.csv')
-# Split data into train and test
-train_data, test_data = split_data(df)
-# Normalization
-scale = MinMaxScaler(feature_range=(0,1))
-scale_train_data = scale.fit_transform(train_data)
-scale_test_data = scale.fit_transform(test_data)
-# Incorporating timesteps into data
-window_size = int(scale_train_data.size*0.3)
-X_train, y_train = incorporate_timesteps(scale_train_data, window_size = window_size)
-X_test, y_test = incorporate_timesteps(scale_test_data, window_size = window_size)
-# Model
-model = Sequential()
-model.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1], 1)))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50,return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50,return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
-model.compile(optimizer='adam',loss='mean_squared_error')
+def create_model(X_train):
+    # Model
+    model = Sequential()
+    model.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50,return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50,return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
+    model.compile(optimizer='adam',loss='mean_squared_error')
+    return model
 
-history = model.fit(X_train,y_train,epochs=25,batch_size=32)
+def run_lstm(X_train):
+    model = create_model(X_train)
+    history = model.fit(X_train,y_train,epochs=25,batch_size=32)
+    model.save_weights('./model/lstm')
 
-model.save_weights('./model/lstm')
-
-result = model.evaluate(X_test, y_test, batch_size=32)
+X_train, y_train, X_test, y_test = preprocessing_data()
+model = create_model(X_train)
+model.load_weights('./model/lstm')
+result = model.predict(X_test )
+rmse = mean_squared_error(result, y_test,squared = False) 
+plot_predictions_vs_true(result, y_test, title="LTSM prediction")
 
 
 
--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_predictions_vs_true(predictions, mid_data):
+def plot_predictions_vs_true(predictions, mid_data, title):
     fig1=plt.figure(figsize=(12, 6))
     plt.plot(mid_data,color='b',label='True')
     plt.plot(predictions,color='orange',label='Prediction')
     plt.xlabel('Date')
     plt.ylabel('Mid Price')
+    plt.title(title)
     plt.legend(fontsize=18)
     plt.show()
 
