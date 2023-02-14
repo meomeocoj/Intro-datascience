80bfda25df8372f199baafc215a865636c757a2b
meomeocoj <tminh1103@gmail.com>
2023-02-14 16:46:01 +0700

    upadte: feature engineering

--- data_handle.py
+++ data_handle.py
@@ -8,17 +8,16 @@ def data_fetching(ticker, start, end):
     # Write data to csv file
     data.to_csv('data/stock_data.csv')
 
-def split_data(df):
+def get_target(df):
     df['Date'] = df['Date'].str.split(' ').str[0]
     df['Date'] = pd.to_datetime(df['Date']).dt.normalize()
     df.sort_values('Date')
-
     high_prices = df.loc[:,"High"].values
     low_prices = df.loc[:,"Low"].values
     mid_prices = (high_prices + low_prices) / 2.0
-    train_data = mid_prices[:int(df.shape[0]*0.8)].reshape(-1,1)
-    test_data = mid_prices[int(df.shape[1]*0.8):].reshape(-1,1)
+    target_train = mid_prices[:int(df.shape[0]*0.8)].reshape(-1,1)
+    target_test = mid_prices[int(df.shape[1]*0.8):].reshape(-1,1)
 
-    return (train_data, test_data)
+    return (target_train, target_test)
 
 
--- lstm.py
+++ lstm.py
@@ -1,39 +1,67 @@
 import numpy as np
 import pandas as pd
 import os
-from sklearn import preprocessing
+import ta
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
-from data_handle import data_fetching, split_data
+from data_handle import data_fetching 
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 from utils import plot_predictions_vs_true
 
 def preprocessing_data():
-# Fetch data
+    # Fetch data
     if not os.path.exists("data/stock_data.csv"):
         data_fetching('AAPL',start='2010-01-01', end='2022-12-31')
-# read csv
+    # read csv
     df = pd.read_csv('data/stock_data.csv')
-# Split data into train and test
-    train_data, test_data = split_data(df)
-# Normalization
+
+    # Get the target of the train processs 
+    df['Mid Average'] = df[['High','Low']].astype(float).mean(axis=1)
+
+    # Feature engineering 
+    window = 20
+
+    df["MA_20"] = df['Adj Close'].rolling(window=window).mean() # Moving average
+    std = df['Adj Close'].rolling(window=window).std()
+
+    df["Upper Band"] = df['MA_20'] + 2*std #Bollinger Band
+    df["Lower Band"] = df['MA_20'] - 2*std
+
+    df['Returns'] = df['Adj Close'].pct_change()
+    df['Volatility'] = df['Returns'].rolling(window=window).std() # Volatility
+
+    df['Daily Returns'] = df['Adj Close'].pct_change() # Daily Return
+
+    # Technical indicators
+    df['RSI'] = ta.momentum.RSIIndicator(df['Adj Close']).rsi() # RSI
+
+    df['MACD'] = ta.trend.MACD(df['Adj Close']).macd() # MACD
+
+    # train_test_split 
+    df_train, df_test = train_test_split(df,test_size=0.1,shuffle = False)
+
+    # Mid average price col num
+    mid_average_col_num = df.columns.get_loc('Mid Average')
+
+    # Normalization
     scale = MinMaxScaler(feature_range=(0,1))
-    scale_train_data = scale.fit_transform(train_data)
-    scale_test_data = scale.fit_transform(test_data)
-# Incorporating timesteps into data
-    window_size = int(scale_train_data.size*0.3)
-    X_train, y_train = incorporate_timesteps(scale_train_data, window_size = window_size)
-    X_test, y_test = incorporate_timesteps(scale_test_data, window_size = window_size)
+    scale_train_data = scale.fit_transform(df_train.drop(columns='Date'))
+    scale_test_data = scale.transform(df_test.drop(columns='Date'))
+
+    # Incorporating timesteps into data
+    X_train, y_train = incorporate_timesteps(scale_train_data, target=mid_average_col_num, window_size = window)
+    X_test, y_test = incorporate_timesteps(scale_test_data, target=mid_average_col_num, window_size = window)
     return X_train, y_train, X_test, y_test
 
 
-def incorporate_timesteps(data,window_size):
+def incorporate_timesteps(data, target,  window_size):
     X_train = []
     y_train = []
-    for i in range(window_size, data.size):
+    for i in range(window_size, len(data)):
         X_train.append(data[i - window_size:i,0])
-        y_train.append(data[i,0])
+        y_train.append(data[i, target])
     X_train, y_train = np.array(X_train), np.array(y_train)
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     return X_train, y_train
@@ -53,16 +81,22 @@ def create_model(X_train):
     model.compile(optimizer='adam',loss='mean_squared_error')
     return model
 
-def run_lstm(X_train):
+def run_lstm(X_train, y_train):
     model = create_model(X_train)
-    history = model.fit(X_train,y_train,epochs=25,batch_size=32)
+    model.fit(X_train,y_train, epochs=25,batch_size=32)
     model.save_weights('./model/lstm')
 
 X_train, y_train, X_test, y_test = preprocessing_data()
+
+
+# run_lstm(X_train, y_train)
+
+
 model = create_model(X_train)
 model.load_weights('./model/lstm')
-result = model.predict(X_test )
+result = model.predict(X_test,)
 rmse = mean_squared_error(result, y_test,squared = False) 
+print(rmse)
 plot_predictions_vs_true(result, y_test, title="LTSM prediction")
 
 
